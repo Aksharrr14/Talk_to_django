fee8724f07806c6dc41c100344c767e41c556776
Akshar Bhatnagar <bhatnagarakshar@gmail.com>
2024-11-26 17:16:38 +0530

    Neon db connection

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
-
+from pgvector.django import VectorField
 # Create your models here.
+
+#OpenAi embeddings 
+EMBEDDING_MODEL ="text-embedding-3-small"
+EMBEDDING_LENGTH = 1536
+
 class BlogPost(models.Model):
     title=models.CharField(max_length=200)
     content=models.TextField()
     timestamp=models.DateTimeField(auto_now_add=True)
+    embedding=VectorField(dimensions=EMBEDDING_LENGTH,blank=True,null=True)  #dimensions using embedding.shape
 
     def __str__(self):
         return self.title
